6bd1ee4a667ee1d207da9ac5a6c2b345b6ad843d
Emma Bukacek <emma.bukacek@gmail.com>
2018-05-16 11:11:58 -0700

    Donezo

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 Javascript bookmark that merges a PR using the squash commit strategy. The commit title
 corresponds to the PR title (including the PR number for easy lookup) and the commit
-message corresponds to changes inside the PR.
+message corresponds to changes inside the PR, denoted either by `## Changes` or `** Changes`,
+followed by either another headline, another bold text, a gif, or until the end of the string.
+
+## Usage
+
+* Make a new bookmark prefixed with `javascript: `.
+* Copy paste `index.js` in front of colon.
+* Click to use!
--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-const searchIndexWaterfall = (text, ...args) => {
-  const matchingRegex = args.find(regex => text.search(regex) !== -1);
-  if (!matchingRegex) return text;
-  const endOfChangesIndex = text.search(matchingRegex);
-  return text.slice(0, endOfChangesIndex).trim();
-};
+(function() {
+  const token = '**Changes**';
+  const searchIndexWaterfall = (text, ...args) => {
+    const matchingRegex = args.find(regex => text.search(regex) !== -1);
+    if (!matchingRegex) return text;
+    const endOfChangesIndex = text.search(matchingRegex);
+    return text.slice(0, endOfChangesIndex).trim();
+  };
 
-module.exports.getEndOfChanges = text => searchIndexWaterfall(
-  text,
-  /.*?(?=\*\*.*\*\*)/,
-  /.*?(?=#+)/,
-  /.*?(?=!\[\])/
-);
+  const getCommitMessage = text => searchIndexWaterfall(
+    text,
+    /.*?(?=\*\*.*\*\*)/,
+    /.*?(?=##)/,
+    /.*?(?=!\[\])/
+  );
 
-const bookmark = () => {
-  const copyMe = (function() {
-    const token = '**Changes**';
-    const everythingAfterChangesTitle = document.querySelector('.js-comment-field')
-      .value
-      .split(token)
-      .pop()
+  const prBody = document.querySelector('.js-comment-field').value;
+  let changesSplit = prBody.split('**Changes**');
+  if (changesSplit.length === 1) changesSplit = prBody.split('## Changes');
 
-    const endOfChanges = getEndOfChanges(everythingAfterChangesTitle);
+  const commitMessage = getCommitMessage(changesSplit.pop());
 
-    document.querySelector('.timeline-comment-wrapper .js-comment-edit-button').click();
-    document.querySelector('.js-comment-cancel-button').click();
-    const prTitle = document.querySelector('.js-issue-title').innerText.trim();
-    const prNumber = document.querySelector('.gh-header-number').innerText.trim();
+  document.querySelector('.timeline-comment-wrapper .js-comment-edit-button').click();
+  document.querySelector('.js-comment-cancel-button').click();
 
-    const mergeButtonEl = document.querySelector('.btn-group-squash .js-details-target');
-    mergeButtonEl.click();
-    document.querySelector('.merge-commit-title').value = `${prTitle} (${prNumber})`;
-    document.querySelector('.merge-commit-message').value = commitMessage;
-    document.querySelector('.merge-branch-form').scrollIntoView();
-  })();
-};
+  const prTitle = document.querySelector('.js-issue-title').innerText.trim();
+  const prNumber = document.querySelector('.gh-header-number').innerText.trim();
+
+  const mergeDropdown = document.querySelector('.btn-group-squash .select-menu-button');
+  mergeDropdown.click();
+
+  const squashAndMergeXpath = "//div[contains(@class, 'select-menu-item') and contains(., 'Squash and merge')]";
+  document.evaluate(squashAndMergeXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+    .singleNodeValue
+    .click();
+
+  const mergeButtonEl = document.querySelector('.btn-group-squash .js-details-target');
+  mergeButtonEl.click();
+
+  document.querySelector('.merge-commit-title').value = `${prTitle} (${prNumber})`;
+  document.querySelector('.merge-commit-message').value = commitMessage;
+  document.querySelector('.merge-branch-form').scrollIntoView();
+})();
